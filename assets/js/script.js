e7b9a8e44c186ba9f3320da541b7d04da668f462
Todd Stuke <tddstuke@gmail.com>
2022-04-03 11:24:48 -0500

    refractored js

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,7 +5,17 @@ var createTaskHandler = function (event) {
   event.preventDefault();
   var taskNameInput = document.querySelector("input[name='task-name']").value;
   var taskTypeInput = document.querySelector("select[name='task-type']").value;
+  // package up data as an object
+  var taskDataObj = {
+    name: taskNameInput,
+    type: taskTypeInput,
+  };
 
+  // send it as an argument to createTaskEl
+  createTaskEl(taskDataObj);
+};
+
+var createTaskEl = function (taskDataObj) {
   //   create list item
   var listItemEl = document.createElement("li");
   listItemEl.className = "task-item";
@@ -19,9 +29,9 @@ var createTaskHandler = function (event) {
   // add HTML content to div
   taskInfoEl.innerHTML =
     "<h3 class='task-name'>" +
-    taskNameInput +
+    taskDataObj.name +
     "</h3><span class='task-type'>" +
-    taskTypeInput +
+    taskDataObj.type +
     "</span>";
   listItemEl.appendChild(taskInfoEl);
 
